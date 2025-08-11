64719295198fba0f793f32c61b3b79fdcd1c981d
erikaishido <118091746+erikaishido@users.noreply.github.com>
2025-08-10 23:10:20 -0700

    rewrite states

--- plant.lua
+++ plant.lua
@@ -12,15 +12,15 @@ function plant.new(planterIndex, plantName)
         T[key] = value
     end
 
-    T.x = global.PLANTOFFSETS[planterIndex].x          -- override xy depending on index
-    T.y = global.PLANTOFFSETS[planterIndex].y
+    T.x = global.PLANT_OFFSETS[planterIndex].x          -- override xy depending on index
+    T.y = global.PLANT_OFFSETS[planterIndex].y
 
 
     if plantName == "" then                       -- RETHINK THIS??????????
         return T
     end
 
-    T.state = 2                                       -- set state to SEEDLING
+    T.state = global.PLANT_STATES.SEEDLING             -- set state to SEEDLING
     T.name = plant.keyToPlant[plantName].name         -- override plant-dependant values
 
     return T
@@ -34,8 +34,8 @@ function plant.initPlantData()
 
     plant.base = {             -- base stats for plants
         name = "",              -- any specified stats will override these values
-        harvestType = 1,
-        state = 1,
+        harvestType = global.HARVEST_TYPES.SINGLE,
+        state = global.PLANT_STATES.EMPTY,
         waterCount = 0,
         isWatered = false,
         x = 0,
@@ -48,11 +48,11 @@ function plant.initPlantData()
 
     plant.tomato = {           -- specific paramerters for each plant
         name = "Tomato",
-        harvestType = 2
+        harvestType = global.HARVEST_TYPES.MULTIPLE
     }
     plant.lettuce = {
         name = "Lettuce",
-        harvestType = 1
+        harvestType = global.HARVEST_TYPES.SINGLE
     }
 
     plant.keyToPlant = {           -- long list of all plants
@@ -67,9 +67,9 @@ end
 -- on-click behavior
 -----------------------------
 function plant.onClick(P)
-    if P.state == 1 then                -- if planter is empty
-        event.openUI("PLANTSELECT")
-    elseif P.state == 5 then
+    if P.state == global.PLANT_STATES.EMPTY then       -- if planter is empty
+        event.openUI("PLANT_SELECT")
+    elseif P.state == global.PLANT_STATES.RIPE then
         plant.harvest(P)
     else
         plant.water(P)
@@ -78,7 +78,7 @@ end
 
 
 function plant.water(P)
-    if P.isWatered == true then         -- plant already watered today
+    if P.isWatered then         -- plant already watered today
         return
     elseif global.WATER < 10 then       -- you are out of water
         return
@@ -90,7 +90,7 @@ end
 
 
 function plant.harvest(P)
-    P.state = 1
+    P.state = global.PLANT_STATES.EMPTY
     P.waterCount = 0
     event.sumResource("food", 30)
 end
@@ -109,17 +109,17 @@ function plant.checkGrowth(P)
 
     P.waterCount = P.waterCount + 1
     -- the unholy if else train
-    if P.state == 2 then
+    if P.state == global.PLANT_STATES.SEEDLING then
         if P.waterCount >= 1 then
-            P.state = 3
+            P.state = global.PLANT_STATES.JUVENILE
         end
-    elseif P.state == 3 then
+    elseif P.state == global.PLANT_STATES.JUVENILE then
         if P.waterCount >= 2 then
-            P.state = 4
+            P.state = global.PLANT_STATES.MATURE
         end
-    elseif P.state == 4 then
+    elseif P.state == global.PLANT_STATES.MATURE then
         if P.waterCount >= 2 then
-            P.state = 5
+            P.state = global.PLANT_STATES.RIPE
         end
     end
 
@@ -133,7 +133,7 @@ end
 
 function plant.draw(P)
 
-    if P.state == 1 then
+    if P.state == global.PLANT_STATES.EMPTY then
         love.graphics.rectangle("line", P.x, P.y, P.w, P.h)
         love.graphics.print("empty planter", P.x+30, P.y+50)
         return
@@ -145,12 +145,12 @@ function plant.draw(P)
 
     love.graphics.print(P.name, P.x+30, P.y+80)
 
-    if P.state == 5 then
+    if P.state == global.PLANT_STATES.RIPE then
         local fruit = love.graphics.newImage(P.ripeSpr)
         love.graphics.draw(fruit, P.x, P.y)
     end
 
-    if P.isWatered == true then
+    if P.isWatered then
         love.graphics.print("watered", (P.x + 50), (P.y - 20) )
     end
 end
